d11b9a145da6102b8b99d2fb634c19b94f67231a
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 12:43:57 +0530

    Add the POST Method: Sending data in the monoogse using Postman.

--- app.js
+++ app.js
@@ -5,15 +5,36 @@ const PORT = 3000;
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const User = require("./models/userSchema");
+const UserProfile = require("./models/userProfileSchema");
 
 mongoose.connect(mongoURI).then(() => console.log("mongoose connected"));
 
-const user =[]
+app.use(express.json())
 
-app.get('/',(req,res)=>{
-    res.json({message:'Connected with mongoes also',user})
-})
+app.post("/user", async (req, res) => {
+  try {
+    const { FirstName, LastName, email, dob, mobile, Password } = req.body;
+    const user = new User({ FirstName, LastName, email, dob, mobile ,Password});
+    const saveUser = await user.save();
 
-app.listen(PORT,()=>{
-    console.log(`server running at http://localhost:${PORT}`)
+    const userprofile = new UserProfile({
+      user_id: saveUser._id,
+      dob,
+      mobile,
+    });
+    await userprofile.save();
+    res.json({
+      message: "User and Profile Created Successfully",
+      user: saveUser,
+      userprofile: userprofile,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+
+app.listen(PORT, () => {
+  console.log(`server running at http://localhost:${PORT}`);
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -6,6 +6,8 @@ const userSchema = mongoose.Schema({
   FirstName: { type: String, require: true },
   LastName: { type: String, require: true },
   email: { type: String, require: true, unique: true },
+  dob: { type: Date, require: true },
+  mobile: Number,
   Password: { type: String, require: true },
 });
 
