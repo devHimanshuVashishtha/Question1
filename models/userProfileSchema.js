765d466aaa599111fd9ab242ea7f38a3bfe24a09
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 11:58:59 +0530

    User Profile Schema is Created

--- models/userProfileSchema.js
+++ models/userProfileSchema.js
@@ -1,1 +1,11 @@
 //this is user profile
+const mongoose = require("mongoose");
+
+const userProfileSchema = mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: "Users" },
+  dob: Date,
+  mobile: Number,
+});
+
+
+module.exports = mongoose.model('UserProfile',userProfileSchema)
