5fc6e59face171810cfe539fac9c25a58a2947d4
devHimanshuVashishtha <himanshuvashishtha001.hp@gmail.com>
2025-07-29 11:47:06 +0530

    User Schema is made in this file

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,12 @@
-// this is user 
+// this is user
+
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  FirstName: { type: String, require: true },
+  LastName: { type: String, require: true },
+  email: { type: String, require: true, unique: true },
+  Password: { type: String, require: true },
+});
+
+module.exports = mongoose.model("User", userSchema);
